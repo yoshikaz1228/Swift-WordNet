77412198dddaee1cbc888d4beede901f5d2b538e
yoshikaz <kunagisatomo.yoshikaz@gmail.com>
2021-10-16 08:21:22 +0900

    fix dependencies.

--- SwiftWordNet/Package.swift
+++ SwiftWordNet/Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftWordNet",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftWordNet",
             targets: ["SwiftWordNet"]),
@@ -21,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftWordNet",
-            dependencies: []),
+            dependencies: [.product(name: "SQLite", package: "SQLite")]),
         .testTarget(
             name: "SwiftWordNetTests",
             dependencies: ["SwiftWordNet"]),
