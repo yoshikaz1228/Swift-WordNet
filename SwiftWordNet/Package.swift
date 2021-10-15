db9a17c6e0cd8ef9b1aea315922c75bbca61d707
yoshikaz <kunagisatomo.yoshikaz@gmail.com>
2021-10-16 08:27:20 +0900

    Update Package.swift

--- SwiftWordNet/Package.swift
+++ SwiftWordNet/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftWordNet",
-            dependencies: [.product(name: "SQLite", package: "SQLite")]),
+            dependencies: [.product(name: "SQLite", package: "SQLite.swift")]),
         .testTarget(
             name: "SwiftWordNetTests",
             dependencies: ["SwiftWordNet"]),
